214a2e917ae30b9b106cb3d6f9e4e09041ad71c8
Suyeon Kang <tndusrkd92@gmail.com>
2020-06-26 12:09:08 +0900

    Add playing sound functionality with play button

--- app.js
+++ app.js
@@ -12,6 +12,33 @@ const app = () => {
     selectTimeBtn: Array.from(document.querySelectorAll('.time-duration button')),
   };
 
+  // Defalut duration 
+  let duration = 600;
+
+  // Get length of outline
+  const outlineLength = DOMstring.outline.getTotalLength();
+
+  // Stop and play sound
+  const checkPlaying = (sound => {
+    if (sound.paused) {
+      sound.play();
+      //DOMstring.video.play();
+      DOMstring.playBtn.src = './img/svg/pause.svg';
+    } else {
+      sound.pause();
+      //DOMstring.video.pause();
+      DOMstring.playBtn.src = './img/svg/play.svg';
+    };
+  });
+
+  // Animate outline of play button 
+  DOMstring.outline.style.strokeDasharray = outlineLength;
+  DOMstring.outline.style.strokeDashoffset = outlineLength;
+
+  // Play sound
+  DOMstring.playBtn.addEventListener('click', () => {
+    checkPlaying(DOMstring.sound);
+  });
 
 };
 
